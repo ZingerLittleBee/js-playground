dd3d0b4bc7741cdd8cf97ee1400b14e81c5b566d
ZingerLittleBee <6970999@gmail.com>
2021-09-06 14:58:16 +0800

    proto

--- prototype/index.js
+++ prototype/index.js
@@ -1,4 +1,4 @@
 function Foo(){}
 var foo = new Foo()
-console.log(foo.__proto__ === Foo.prototype)
-console.log(Foo.prototype.__proto__ === Object.prototype)
+console.log(foo.__proto__ === Foo.prototype) // true
+console.log(Foo.prototype.__proto__ === Object.prototype) // true
