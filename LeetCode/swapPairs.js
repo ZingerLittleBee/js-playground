8f88a4e3f5bdd127d9ccb24226de93627186747f
ZingerLittleBee <6970999@gmail.com>
2022-06-24 00:01:22 +0800

    LeetCode: 24

---
+++ LeetCode/swapPairs.js
@@ -0,0 +1,26 @@
+/*
+24. 两两交换链表中的节点
+给你一个链表，两两交换其中相邻的节点，并返回交换后链表的头节点。你必须在不修改节点内部的值的情况下完成本题（即，只能进行节点交换）。
+https://leetcode.cn/problems/swap-nodes-in-pairs/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+	if (!head || !head.next) return head
+	let one = head
+	let two = head.next
+	let three = two.next
+	two.next = one
+	one.next = swapPairs(three)
+	return two
+}
