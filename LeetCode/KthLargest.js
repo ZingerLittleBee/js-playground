40be9eb56d11e42c8cf9335fe343ca1d8884ad08
ZingerLittleBee <6970999@gmail.com>
2022-06-27 23:44:29 +0800

    LeetCode: 703

---
+++ LeetCode/KthLargest.js
@@ -0,0 +1,125 @@
+/*
+703. 数据流中的第 K 大元素
+https://leetcode.cn/problems/kth-largest-element-in-a-stream/
+设计一个找到数据流中第 k 大元素的类（class）。注意是排序后的第 k 大元素，不是第 k 个不同的元素。
+
+请实现 KthLargest 类：
+KthLargest(int k, int[] nums) 使用整数 k 和整数流 nums 初始化对象。
+int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 k 大的元素。
+
+提示：
+1 <= k <= 104
+0 <= nums.length <= 104
+-104 <= nums[i] <= 104
+-104 <= val <= 104
+最多调用 add 方法 104 次
+题目数据保证，在查找第 k 大元素时，数组中至少有 k 个元素
+ */
+
+/**
+ * @param {number} k
+ * @param {number[]} nums
+ */
+var KthLargest = function(k, nums) {
+    this.k = k
+    this.heap = new MinHeap()
+    for (const x of nums) {
+        this.add(x);
+    }
+};
+
+/**
+ * @param {number} val
+ * @return {number}
+ */
+KthLargest.prototype.add = function(val) {
+    this.heap.offer(val)
+    if (this.heap.size() > this.k) {
+        this.heap.pop()
+    }
+    return this.heap.peek()
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * var obj = new KthLargest(k, nums)
+ * var param_1 = obj.add(val)
+ */
+
+
+class MinHeap {
+    constructor(data = []) {
+        this.data = data
+        this.heapify()
+    }
+    heapify() {
+        if (this.size() < 2) return
+        for (let i = 0; i < this.size(); i++) {
+            this.bubbleUp(i)
+        }
+    }
+    size() {
+        return this.data.length
+    }
+    isEmpty() {
+        return this.data.length === 0
+    }
+    swap(index1, index2) {
+        [this.data[index1], this.data[index2]] = [this.data[index2], this.data[index1]]
+    }
+    pop() {
+        if (this.size() === 0) return null
+        const result = this.data[0]
+        const last = this.data.pop()
+        if (this.size() !== 0) {
+            this.data[0] = last
+            this.bubbleDown(0)
+        }
+        return result
+    }
+    peek() {
+        if (this.size() === 0) return null
+        return this.data[0]
+    }
+    bubbleUp(index) {
+        while(index > 0) {
+            let parentIndex = index >> 1
+            if (this.data[index] < this.data[parentIndex]) {
+                this.swap(index, parentIndex)
+                index = parentIndex
+            } else {
+                break
+            }
+        }
+    }
+    bubbleDown(index) {
+        const lastIndex = this.size() - 1;
+        while (true) {
+            const leftIndex = index * 2 + 1;
+            const rightIndex = index * 2 + 2;
+            let findIndex = index;
+            if (
+                leftIndex <= lastIndex &&
+                this.data[leftIndex] - this.data[findIndex] < 0
+            ) {
+                findIndex = leftIndex;
+            }
+            if (
+                rightIndex <= lastIndex &&
+                this.data[rightIndex] - this.data[findIndex] < 0
+            ) {
+                findIndex = rightIndex;
+            }
+            if (index !== findIndex) {
+                this.swap(index, findIndex);
+                index = findIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    offer(val) {
+        this.data.push(val)
+        this.bubbleUp(this.size() - 1)
+    }
+}
