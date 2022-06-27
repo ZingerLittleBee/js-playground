96252eecd5530b4e3b93c96671da44e4b18d0fda
ZingerLittleBee <6970999@gmail.com>
2022-06-27 23:53:18 +0800

    LeetCode: 703

--- LeetCode/KthLargest.js
+++ LeetCode/KthLargest.js
@@ -21,77 +21,70 @@ int add(int val) 将 val 插入数据流 nums 后，返回当前数据流中第 
  * @param {number[]} nums
  */
 var KthLargest = function(k, nums) {
-    this.k = k
-    this.heap = new MinHeap()
+    this.k = k;
+    this.heap = new MinHeap();
     for (const x of nums) {
         this.add(x);
     }
 };
 
-/**
- * @param {number} val
- * @return {number}
- */
 KthLargest.prototype.add = function(val) {
-    this.heap.offer(val)
+    this.heap.offer(val);
     if (this.heap.size() > this.k) {
-        this.heap.pop()
+        this.heap.poll();
     }
-    return this.heap.peek()
+    return this.heap.peek();
 };
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * var obj = new KthLargest(k, nums)
- * var param_1 = obj.add(val)
- */
-
 
 class MinHeap {
     constructor(data = []) {
-        this.data = data
-        this.heapify()
+        this.data = data;
+        this.comparator = (a, b) => a - b;
+        this.heapify();
     }
+
     heapify() {
-        if (this.size() < 2) return
-        for (let i = 0; i < this.size(); i++) {
-            this.bubbleUp(i)
+        if (this.size() < 2) return;
+        for (let i = 1; i < this.size(); i++) {
+            this.bubbleUp(i);
         }
     }
-    size() {
-        return this.data.length
-    }
-    isEmpty() {
-        return this.data.length === 0
-    }
-    swap(index1, index2) {
-        [this.data[index1], this.data[index2]] = [this.data[index2], this.data[index1]]
-    }
-    pop() {
-        if (this.size() === 0) return null
-        const result = this.data[0]
-        const last = this.data.pop()
-        if (this.size() !== 0) {
-            this.data[0] = last
-            this.bubbleDown(0)
-        }
-        return result
-    }
+
     peek() {
-        if (this.size() === 0) return null
-        return this.data[0]
+        if (this.size() === 0) return null;
+        return this.data[0];
     }
+
+    offer(value) {
+        this.data.push(value);
+        this.bubbleUp(this.size() - 1);
+    }
+
+    poll() {
+        if (this.size() === 0) {
+            return null;
+        }
+        const result = this.data[0];
+        const last = this.data.pop();
+        if (this.size() !== 0) {
+            this.data[0] = last;
+            this.bubbleDown(0);
+        }
+        return result;
+    }
+
     bubbleUp(index) {
-        while(index > 0) {
-            let parentIndex = index >> 1
-            if (this.data[index] < this.data[parentIndex]) {
-                this.swap(index, parentIndex)
-                index = parentIndex
+        while (index > 0) {
+            const parentIndex = (index - 1) >> 1;
+            if (this.comparator(this.data[index], this.data[parentIndex]) < 0) {
+                this.swap(index, parentIndex);
+                index = parentIndex;
             } else {
-                break
+                break;
             }
         }
     }
+
     bubbleDown(index) {
         const lastIndex = this.size() - 1;
         while (true) {
@@ -100,13 +93,13 @@ class MinHeap {
             let findIndex = index;
             if (
                 leftIndex <= lastIndex &&
-                this.data[leftIndex] - this.data[findIndex] < 0
+                this.comparator(this.data[leftIndex], this.data[findIndex]) < 0
             ) {
                 findIndex = leftIndex;
             }
             if (
                 rightIndex <= lastIndex &&
-                this.data[rightIndex] - this.data[findIndex] < 0
+                this.comparator(this.data[rightIndex], this.data[findIndex]) < 0
             ) {
                 findIndex = rightIndex;
             }
@@ -118,8 +111,12 @@ class MinHeap {
             }
         }
     }
-    offer(val) {
-        this.data.push(val)
-        this.bubbleUp(this.size() - 1)
+
+    swap(index1, index2) {
+        [this.data[index1], this.data[index2]] = [this.data[index2], this.data[index1]];
+    }
+
+    size() {
+        return this.data.length;
     }
 }
