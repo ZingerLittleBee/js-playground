cd7d6f3bcb91eaec306007bde7ba27884b9bdd51
ZingerLittleBee <6970999@gmail.com>
2022-06-29 23:49:23 +0800

    LeetCode: 239

---
+++ LeetCode/maxSlidingWindow.js
@@ -0,0 +1,45 @@
+
+/*
+239. 滑动窗口最大值
+给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k个数字。滑动窗口每次只向右移动一位。
+返回 滑动窗口中的最大值 。
+
+提示：
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+1 <= k <= nums.length
+
+https://leetcode.cn/problems/sliding-window-maximum/
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    const n = nums.length;
+    const q = [];
+    for (let i = 0; i < k; i++) {
+        while (q.length && nums[i] >= nums[q[q.length - 1]]) {
+            q.pop();
+        }
+        q.push(i);
+    }
+
+    const ans = [nums[q[0]]];
+    for (let i = k; i < n; i++) {
+        while (q.length && nums[i] >= nums[q[q.length - 1]]) {
+            q.pop();
+        }
+        q.push(i);
+        while (q[0] <= i - k) {
+            q.shift();
+        }
+        ans.push(nums[q[0]]);
+    }
+    return ans;
+};
+
+// test
+console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3))
