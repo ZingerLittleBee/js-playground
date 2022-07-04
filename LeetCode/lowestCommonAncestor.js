48940cd92c4ba80ec9a2bd35935500b7bccb66f8
ZingerLittleBee <6970999@gmail.com>
2022-07-04 18:00:46 +0800

    LeetCode: 236

---
+++ LeetCode/lowestCommonAncestor.js
@@ -0,0 +1,40 @@
+/*
+236. 二叉树的最近公共祖先
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+提示：
+树中节点数目在范围 [2, 105] 内。
+-109 <= Node.val <= 109
+所有 Node.val 互不相同 。
+p != q
+p 和 q 均存在于给定的二叉树中。
+
+https://leetcode.cn/problems/lowest-common-ancestor-of-a-binary-tree/
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (root === null || root.val === p.val || root.val === q.val) return root
+    const left = lowestCommonAncestor(root.left, p, q)
+    const right = lowestCommonAncestor(root.right, p, q)
+    if (left === null) {
+        return right
+    }
+    if (right === null) {
+        return left
+    }
+    return root
+};
